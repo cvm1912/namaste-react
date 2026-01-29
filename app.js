6ed9a42f2cdb2ad6df2272a3b7bd4bf93b31106e
cvm1912 <rajshivam1912@gmail.com>
2026-01-29 16:21:08 +0530

    file sturcuture

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
 const rooot =  React.createElement('div',{id:"parents"},
-                            React.createElement('div',{id:"child"},
-                                [React.createElement('h1',{}, 'welcome to React'),React.createElement('h1',{}, 'Namaste React')] ))
-ReactDOM.render(rooot, document.getElementById('root'));
+                                [React.createElement('h1',{key: 1}, 'welcome to React'),React.createElement('h1',{key: 2}, 'Namaste React')] )
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(rooot);
       
