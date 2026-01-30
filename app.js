fd3c793f57d2aaba721c5522f9b137ec4ff6fafc
cvm1912 <rajshivam1912@gmail.com>
2026-01-31 01:01:31 +0530

    components

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-const jsxheading =(
+const Title = () => (
     <h1 className="">
         Namaste React using JSX
     </h1>
 ) 
 
+const HeadingComponent = () =>{
+    return <h1>Hello , this is JS</h1>
+}
+
+const HeadingComponent2 = () =>(
+    <div>
+        <Title/>
+        <HeadingComponent/>
+        <h1>Hello , this is JSX</h1>
+    </div>
+   
+)
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(jsxheading)
+root.render(<HeadingComponent2 />)
+
+
 
 
